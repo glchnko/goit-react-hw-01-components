cd7a0cc7fe42577ffa76193d2ba15cfea8fa0d02
Anna <agalucnko@gmail.com>
2023-01-24 00:37:19 +0200

    add change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,10 @@ import user from "../data/user.json"
 import Statistics from "./Statistics/Statistics";
 import data from "../data/data.json";
 import FriendList from "./FriendsList/FriendList";
-
 import friends from "../data/friends.json";
-
-
 import TransactionHistory from "./TransactionHistory/TransactionHistory";
 import transactions from "../data/transactions.json";
-import FriendItem from "./FriendsList/FriendItem";
+
 
 
 
