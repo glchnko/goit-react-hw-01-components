216b0a25311eb56afc6a82e93be68b1a810f2112
Anna <agalucnko@gmail.com>
2023-01-24 00:41:33 +0200

    add styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 import s from './profile.module.css';
 
 
-
 const Profile = ({name, tag, location, avatar, stats}) => {
     return (
         <div className={s.profile}>
